43f46f07be5f1174e2ceed4786a723cd4167a56c
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-11-30 22:55:53 -0300

    Criando as tabelas

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,20 +1,15 @@
-const express = require('express');
-const consing = require('consign');
-const consign = require('consign/lib/consign');
+const express = require("express");
+const consing = require("consign");
+const consign = require("consign/lib/consign");
 
-const bodyParser = require('body-parser');
-
-
+const bodyParser = require("body-parser");
 
 module.exports = () => {
-    const app = express()
-    app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.json())
-    
-    consign()
-    .include('controller')
-    .into(app);
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-    return app
-}
+  consign().include("controller").into(app);
 
+  return app;
+};
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const customExpress = require("./config/customExpress");
 
 const conexao = require("./infraestrutura/conexao");
+const tabelas = require("./infraestrutura/tabelas");
 conexao.connect((erro) => {
   if (erro) {
     console.log(erro);
   } else {
-    const app = customExpress();
-
-    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
     console.log("Banco conectado com sucesso");
+    tabelas.init(conexao);
+    const app = customExpress();
+    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
   }
 });
