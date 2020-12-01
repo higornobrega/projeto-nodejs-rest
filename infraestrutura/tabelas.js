bc3e4ede17df13af873f050fc50b78eb19643f02
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-11-30 23:28:34 -0300

    Persistindo os dados

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -7,7 +7,7 @@ class Tabelas {
   }
   criarAtendimentos() {
     const sql =
-      "CREATE TABLE Atendimentos(id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20), servico VARCHAR(20) NOT NULL, status VARCHAR(20) NOT NULL, observacoes TEXT, PRIMARY KEY(id))";
+      "CREATE TABLE IF NOT EXISTS Atendimentos(id int NOT NULL AUTO_INCREMENT, cliente VARCHAR(50) NOT NULL, pet VARCHAR(20), servico VARCHAR(20) NOT NULL, status VARCHAR(20) NOT NULL, observacoes TEXT, PRIMARY KEY(id))";
 
     this.conexao.query(sql, (erro) => {
       if (erro) {
