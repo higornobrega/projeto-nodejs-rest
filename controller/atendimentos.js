e216f146d1feb2d2189ba017a99a93e8602277aa
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-12-10 22:24:41 -0300

    Adicionando campos data e agendamento

--- controller/atendimentos.js
+++ controller/atendimentos.js
@@ -6,8 +6,6 @@ module.exports = (app) => {
   );
   app.post("/atendimentos", (req, res) => {
     const atendimento = req.body;
-    Atendimento.adiciona(atendimento);
-    console.log(req.body);
-    res.send("Você está na rota de atendimentos e está realizando um POST");
+    Atendimento.adiciona(atendimento, res);
   });
 };
