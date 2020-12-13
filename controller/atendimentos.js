946d0574f180e61c99b650a45625ffaf0537d178
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-12-13 10:24:09 -0300

    Outros verbos HTTP-GET

--- controller/atendimentos.js
+++ controller/atendimentos.js
@@ -1,11 +1,25 @@
 const Atendimento = require("../models/atendimentos");
 
 module.exports = (app) => {
-  app.get("/atendimentos", (req, res) =>
-    res.send("Você está na rota de atendimentos e está realizando um GET")
-  );
+  app.get("/atendimentos", (req, res) =>{
+    Atendimento.lista(res) //Chamar a listagem do models enviando o res
+  });
+  
+app.get('/atendimentos/:id', (req, res) => { //Novo get para mostrar por id
+  const id = parseInt(req.params.id) //Converter o id para inteiro
+  Atendimento.buscarPorId(id, res) //Chamar o buscaPorId do models enviando id e res
+})
+
   app.post("/atendimentos", (req, res) => {
     const atendimento = req.body;
     Atendimento.adiciona(atendimento, res);
   });
 };
+
+/*
+  GET
+  *Chamar a listagem do models enviando o res
+  *Novo get para mostrar por id
+    *Converter o id para inteiro
+    *Chamar o buscaPorId do models enviando id e res
+*/
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -39,5 +39,40 @@ class Atendimento {
     }
 
   }
+
+  lista(res){ //Criar método de listar que recebe res
+    const sql = 'SELECT* FROM atendimentos'; //*Criar variavel para receber a seleção de todos dados
+    conexao.query(sql, (erro, resultados) => { //criar a conecção do o BD e a variavel de de seleção
+      if(erro){ //tratamento de erro
+        res.staus(400).json(erro)
+      }else {
+        res.status(200).json(resultados)
+      }
+    })
+  }
+
+  buscarPorId(id, res){
+    const sql = `SELECT * FROM atendimentos WHERE id=${id}`
+    conexao.query(sql, (erro, resultados) => {
+      const atendimento = resultados[0];
+      if(erro){
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(atendimento);
+      }
+    })
+  }
 }
 module.exports = new Atendimento();
+
+  /*GET
+  *Criar método de listar que recebe res
+    *Criar variavel para receber a seleção de todos dados
+    *criar a conecção do o BD e a variavel de de seleção
+    *tratamento de erro
+  *Criar método buscarPorId que recebe o id e res
+    *Criar variavel para receber a seleção de dados pelo id
+    *Tratamento de erro
+
+
+  */
