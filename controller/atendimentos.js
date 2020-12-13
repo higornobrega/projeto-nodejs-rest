2bed2a814728531372d23c44c82417cc2b9c4f39
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-12-13 11:29:02 -0300

    UPDATE

--- controller/atendimentos.js
+++ controller/atendimentos.js
@@ -14,7 +14,15 @@ app.get('/atendimentos/:id', (req, res) => { //Novo get para mostrar por id
     const atendimento = req.body;
     Atendimento.adiciona(atendimento, res);
   });
+
+  app.patch('/atendimentos/:id', (req, res) => { //Criar app.patch com '/atendimentos/:id
+    const id = parseInt(req.params.id) //Converter id para int
+    const valores = req.body //Criar variável para receber os valores da requisição do body
+    Atendimento.altera(id, valores, res) //Chamar o altera passando o id, valores e a resposta
+  });
 };
+
+
 
 /*
   GET
@@ -22,4 +30,13 @@ app.get('/atendimentos/:id', (req, res) => { //Novo get para mostrar por id
   *Novo get para mostrar por id
     *Converter o id para inteiro
     *Chamar o buscaPorId do models enviando id e res
+*/
+/*UPDATE
+  *Diferença de PUT para PATCH
+  *PUT: Quando vier ou for mudar todo o objeto
+  *PATCH: Quando for mudar apenas um atributo do objeto
+  *Criar app.patch com '/atendimentos/:id
+    *Converter id para int
+    *Criar variável para receber os valores da requisição do body
+    *Chamar o altera passando o id, valores e a resposta
 */
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,32 +3,33 @@ const conexao = require("../infraestrutura/conexao");
 
 class Atendimento {
   adiciona(atendimento, res) {
-    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-    const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
-    const clienteEhValido = atendimento.cliente.length >=5;
+    const dataCriacao = moment().format("YYYY-MM-DD HH:MM:SS");
+    const data = moment(atendimento.data, "DD/MM/YYYY").format(
+      "YYYY-MM-DD HH:MM:SS"
+    );
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 5;
 
     const validacoes = [
       {
-        nome: 'data',
+        nome: "data",
         valido: dataEhValida,
-        mensagem:'Data deve ser maior ou igual a data atual'
+        mensagem: "Data deve ser maior ou igual a data atual",
       },
       {
-        nome:'cliente',
+        nome: "cliente",
         valido: clienteEhValido,
-        mensagem:'Cliente deve ter pelo menos cinco caracteres'
-      }
-    ]
-    const erros = validacoes.filter(campo => !campo.valido)
-    const existemErros = erros.length
+        mensagem: "Cliente deve ter pelo menos cinco caracteres",
+      },
+    ];
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
     if (existemErros) {
-      res.status(400).json(erros)
+      res.status(400).json(erros);
     } else {
-      const atendimentoDatado = {...atendimento, dataCriacao, data}
-      const sql = "INSERT INTO Atendimentos SET ?"
-   
-  
+      const atendimentoDatado = { ...atendimento, dataCriacao, data };
+      const sql = "INSERT INTO Atendimentos SET ?";
+
       conexao.query(sql, atendimentoDatado, (erro, resultados) => {
         if (erro) {
           res.status(400).json(erro);
@@ -37,42 +38,67 @@ class Atendimento {
         }
       });
     }
-
   }
 
-  lista(res){ //Criar método de listar que recebe res
-    const sql = 'SELECT* FROM atendimentos'; //*Criar variavel para receber a seleção de todos dados
-    conexao.query(sql, (erro, resultados) => { //criar a conecção do o BD e a variavel de de seleção
-      if(erro){ //tratamento de erro
-        res.staus(400).json(erro)
-      }else {
-        res.status(200).json(resultados)
+  lista(res) {
+    //Criar método de listar que recebe res
+    const sql = "SELECT* FROM atendimentos"; //*Criar variavel para receber a seleção de todos dados
+    conexao.query(sql, (erro, resultados) => {
+      //criar a conecção do o BD e a variavel de de seleção
+      if (erro) {
+        //tratamento de erro
+        res.staus(400).json(erro);
+      } else {
+        res.status(200).json(resultados);
       }
-    })
+    });
   }
 
-  buscarPorId(id, res){
-    const sql = `SELECT * FROM atendimentos WHERE id=${id}`
+  buscarPorId(id, res) {
+    const sql = `SELECT * FROM atendimentos WHERE id=${id}`;
     conexao.query(sql, (erro, resultados) => {
       const atendimento = resultados[0];
-      if(erro){
+      if (erro) {
         res.status(400).json(erro);
       } else {
         res.status(200).json(atendimento);
+      }
+    });
+  }
+
+  altera(id, valores, res){ //Criar método altera que recebe id, valores e res
+    if(valores.data){ //Se existir a data nos valores
+      valores.data = moment(valores.data, "DD/MM/YYYY").format(
+        "YYYY-MM-DD HH:MM:SS"
+      ); //Converter a data
+    }
+    const sql = 'UPDATE atendimentos SET ? WHERE id=?' //Criar a variável que recebe o UPDATE e set alguma coisa de algum id
+    conexao.query(sql, [valores, id], (erro, resultados) => { //Cria conexao com as variáveis necessárias para o update
+      if(erro){ //Tratamento de erro
+        res.status(400).json(erro)
+      } else {
+        res.status(200).json(resultados)
       }
     })
   }
 }
 module.exports = new Atendimento();
 
-  /*GET
-  *Criar método de listar que recebe res
-    *Criar variavel para receber a seleção de todos dados
-    *criar a conecção do o BD e a variavel de de seleção
-    *tratamento de erro
-  *Criar método buscarPorId que recebe o id e res
-    *Criar variavel para receber a seleção de dados pelo id
-    *Tratamento de erro
+/*GET
+ *Criar método de listar que recebe res
+ *Criar variavel para receber a seleção de todos dados
+ *criar a conecção do o BD e a variavel de de seleção
+ *tratamento de erro
+ *Criar método buscarPorId que recebe o id e res
+ *Criar variavel para receber a seleção de dados pelo id
+ *Tratamento de erro
+ */
+/*UPDATE
+ *Criar método altera que recebe id, valores e res
+  *Criar a variável que recebe o UPDATE e set alguma coisa de algum id
+  *Cria conexao com as variáveis necessárias para o update
+  *Tratamento de erro
+  *Se existir a data nos valores
+  *Converter a data
 
-
-  */
+*/
